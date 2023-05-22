c9db51c0f3df9d4c59410e33aac9a53d30926f15
Jude <judeolaolu147@yahoo.com>
2023-05-22 07:41:48 +0100

    corected activators (sigmoid & tanh)

--- util/Activators.c
+++ util/Activators.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <jCG.h>
 #include "Activators.h"
 #include "Activators.r.h"
 
@@ -26,5 +27,5 @@ static void *tanh(void *x)
 }
 static void *sig(void *x)
 {
-    return NULL;
+    return jCG.divide(jCG.one, jCG.add(jCG.one, jCG.exp(jCG.neg(x))));
 }
